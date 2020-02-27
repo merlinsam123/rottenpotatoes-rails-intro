9bec73ad53c875b0b138cb3dfeb44193da89ec23
EC2 Default User <ec2-user@ip-172-31-84-108.ec2.internal>
2020-02-27 07:28:35 +0000

    part 3 complete

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -11,7 +11,47 @@ class MoviesController < ApplicationController
   end
 
   def index
-    @movies = Movie.all
+    # By default, all ratings appear
+    @all_ratings = ["G","PG","PG-13","R"]
+   
+    # if this is the user's first time accessing the page, session[:ratings]
+    # will be empty and thus needs to have the full list put into it so that
+    # all movies show up
+    if(!session[:ratings])
+      session[:ratings] = @all_ratings
+    end
+   
+    # Takes ratings_choice from params based on what the user does
+    @ratings_choice = params[:ratings]
+    
+    # If there is nothing in ratings_choice, that means the user
+    # attempted a search with no ratings. In this case, the
+    # value of ratings_choice is taken from the last valid session
+    if(!@ratings_choice)
+      @ratings_choice = session[:ratings]
+    end
+    
+    # Saves the values of ratings_choice in case the user leaves the page
+    session[:ratings] = @ratings_choice
+    
+    # Takes sort_by from params based on what the user does
+    @sort_by = params[:sort]
+    
+    # Gets sort from session if nothing is in sort_by
+    if(!@sort_by)
+      @sort_by = session[:sort]
+    end
+    
+    # Saves the values of sort_by in case the user leaves the page
+    session[:sort] = @sort_by
+    
+    # Displays movies according to specifications
+    @movies = Movie.where(rating: @ratings_choice.keys).order(@sort_by)
+    
+    #if(!params[:ratings] and !session[:ratings])
+      #flash.keep
+      #redirect_to movies_path(:sort => session[:sort], :ratings => session[:ratings])
+    #end
   end
 
   def new
